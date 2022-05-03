8662f4863ca3e6a33f69855456df612e1a09079d
seatown88 <haythorne@comcast.net>
2022-05-03 09:45:30 -0700

    update

---
+++ 05_python_for_devops/lab_str_func.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+s = "abcdefghijklmnopqrstuvwxyz"
+usr_prompt = input("Enter a string value: ")
+usr_stride = int(input("Enter a stride value:"))
+print("You entered", usr_prompt)
+
